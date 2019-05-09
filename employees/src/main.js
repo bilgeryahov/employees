ba1bbe85a0c85438931c582e8b43a9f63c73ca3f
bilger-progress <bilger.yahov@progress.com>
2019-05-09 16:43:50 +0300

    Basic components and routing in place.

--- employees/src/main.js
+++ employees/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
 import App from './components/App';
+import VueRouter from 'vue-router';
+
+import Dashboard from './components/Dashboard';
+import Signin from './components/Signin';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/dashboard', component: Dashboard },
+        { path: '/signin', component: Signin }
+    ]
+});
 
 new Vue({
     el: '#app',
+    router,
     render: h => h(App)
 });
