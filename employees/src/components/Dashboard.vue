4a42a0d1942301ae4c5b5e9723cd0adce73dd0f6
bilger-progress <bilger.yahov@progress.com>
2019-05-09 18:15:27 +0300

    Fetch the data for all of the employees.

--- employees/src/components/Dashboard.vue
+++ employees/src/components/Dashboard.vue
@@ -1,20 +1,36 @@
 <template>
-    <div class="">
-        <h3>Dashboard</h3>
-         <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
-        {{$store.state}}
-    </div>
+  <div class>
+    <h3>Dashboard</h3>
+    <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
+    {{$store.state}}
+  </div>
 </template>
 
 <script>
-    import { firebaseApp } from '../firebaseApp';
+import { firebaseApp, secretRef } from "../firebaseApp";
 
-    export default {
-        methods: {
-            signOut() {
-                this.$store.dispatch('signOut');
-                firebaseApp.auth().signOut();
-            }
-        }
+export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch("signOut");
+      firebaseApp.auth().signOut();
+    },
+    fetchEmployees(params) {
+      const url = params.employeesEndpoint;
+      const headers = {
+        Authorization: params.authorizationHeader,
+        "Content-Type": "application/json"
+      };
+      fetch(url, {
+        method: "GET",
+        headers
+      })
+        .then(res => res.json())
+        .then(jsonRes => console.log(jsonRes));
     }
+  },
+  mounted() {
+    secretRef.on("value", snap => this.fetchEmployees(snap.val()));
+  }
+};
 </script>
