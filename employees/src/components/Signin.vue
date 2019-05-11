174c71d3a94828cbf38206c81b20148c48b0e937
bilger-progress <bilger.yahov@progress.com>
2019-05-11 11:48:47 +0300

    Code formatting.

--- employees/src/components/Signin.vue
+++ employees/src/components/Signin.vue
@@ -1,45 +1,37 @@
 <template>
-	<div class="from-inline">
-		<h3>Sign In</h3>
-		<div class="form-group">
-			<input
-				type="text"
-				placeholder="email"
-				class="form-control"
-				v-model="email"
-			/>
-			<input
-				type="password"
-				placeholder="password"
-				class="form-control"
-				v-model="password"
-			/>
-			<button class="btn btn-primary signin-btn" @click="signIn">Sign In</button>
-		</div>
-		<br />
-		<p>{{ error.message }}</p>
-		<br />
-	</div>
+  <div class="from-inline">
+    <h3>Sign In</h3>
+    <div class="form-group">
+      <input type="text" placeholder="email" class="form-control" v-model="email">
+      <input type="password" placeholder="password" class="form-control" v-model="password">
+      <button class="btn btn-primary signin-btn" @click="signIn">Sign In</button>
+    </div>
+    <br>
+    <p>{{ error.message }}</p>
+    <br>
+  </div>
 </template>
 
 <script>
-    import { firebaseApp } from '../firebaseApp';
+import { firebaseApp } from "../firebaseApp";
 
-    export default {
-        data() {
-            return {
-                email: '',
-                password: '',
-                error: {
-                    message: ''
-                }
-            }
-        },
-        methods: {
-            signIn() {
-                firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
-                .catch(error => this.error = error);
-            }
-        }
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: {
+        message: ""
+      }
+    };
+  },
+  methods: {
+    signIn() {
+      firebaseApp
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .catch(error => (this.error = error));
     }
+  }
+};
 </script>
