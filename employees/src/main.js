8b334177e995150866ba7aa0b7ea626b2ad80a73
bilger-progress <bilger.yahov@progress.com>
2019-05-11 11:48:37 +0300

    Set-up routing for the new Employee component. Make sure to navigate to the proper routes when authenticated.

--- employees/src/main.js
+++ employees/src/main.js
@@ -5,6 +5,11 @@ import { firebaseApp } from './firebaseApp';
 
 import Dashboard from './components/Dashboard';
 import Signin from './components/Signin';
+import Employee from './components/Employee';
+
+const DASHBOARD_ROUTE = '/dashboard';
+const SIGNIN_ROUTE = '/signin';
+const EMPLOYEE_ROUTE = '/employee/:id';
 
 import store from './store';
 
@@ -13,16 +18,26 @@ Vue.use(VueRouter);
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/dashboard', component: Dashboard },
-        { path: '/signin', component: Signin }
+        { path: DASHBOARD_ROUTE, component: Dashboard },
+        { path: SIGNIN_ROUTE, component: Signin },
+        { path: EMPLOYEE_ROUTE, component: Employee }
     ]
 });
+
+const allowedAuthRoutes = [
+    DASHBOARD_ROUTE,
+    EMPLOYEE_ROUTE
+];
 
 firebaseApp.auth().onAuthStateChanged(user => {
     if (user) {
         user.getIdToken(true)
             .then(fetchedIdToken => store.dispatch('signIn', { ...user, fetchedIdToken }))
-            .then(() => router.push('/dashboard'));
+            .then(() => {
+                if (!router.currentRoute.matched[0] || !allowedAuthRoutes.includes(router.currentRoute.matched[0].path) ) {
+                    router.push(DASHBOARD_ROUTE);
+                }
+            });
     } else {
         router.replace('/signin');
     }
